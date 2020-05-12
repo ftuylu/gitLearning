921e53e18282bd2f1a3ceae628c46b4e0e39f7ac
Fatma Tuylu <62037756+ftuylu@users.noreply.github.com>
2020-05-12 16:05:16 +0100

    Update Practice.java

--- src/tests/Practice.java
+++ src/tests/Practice.java
@@ -4,6 +4,7 @@ public class Practice {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("second");
+        System.out.println("this line inserted from github");
     }
 
 
